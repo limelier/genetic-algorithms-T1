9c82a42c5dd1c109d18fe571c12b68912142184d
limelier <tudor.iacobescu.1@gmail.com>
2019-10-19 20:54:05 +0300

    Added mergeBitsets and splitBitset.

--- util/bitsetFuncs.cpp
+++ util/bitsetFuncs.cpp
@@ -7,8 +7,8 @@
 #include <random>
 
 bool randomBool() {
-    static std::random_device random_device;
-    static thread_local auto generator = std::mt19937_64(random_device());
+    static std::random_device randomDevice;
+    static thread_local auto generator = std::mt19937_64(randomDevice());
     static auto gen = std::bind(std::uniform_int_distribution<>(0,1), generator);
 
     return gen();
@@ -54,3 +54,31 @@ double bitsetToDouble(const std::vector<bool>& bitset, size_t bits, double lower
 
     return result;
 }
+std::vector<bool> mergeBitsets(const std::vector<std::vector<bool>>& bitsets) {
+    std::vector<bool> result;
+    const size_t chunks = bitsets.size();
+    const size_t chunkSize = bitsets[0].size();
+    const size_t size = chunks * chunkSize;
+
+    result.reserve(size);
+    for (auto bitset : bitsets) {
+        result.insert(result.end(), bitset.begin(), bitset.end());
+    }
+
+    return result;
+}
+std::vector<std::vector<bool>> splitBitset(std::vector<bool> bitset, size_t chunkSize) {
+    std::vector<std::vector<bool>> result;
+    const size_t chunks = bitset.size() / chunkSize;
+
+    for (size_t i = 0; i < chunks; i++) {
+        const size_t chunkStart = i * chunkSize;
+        const size_t chunkEnd = (i + 1) * chunkSize;
+
+        result.emplace_back(bitset.begin() + chunkStart, bitset.begin() + chunkEnd);
+    }
+
+    return result;
+}
+
+
